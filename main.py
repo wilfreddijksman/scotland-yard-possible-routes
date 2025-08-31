34937075a567d34ea53aec8a6a7d24344c469533
Wilfred Dijksman <1773594+wilfreddijksman@users.noreply.github.com>
2025-08-31 21:59:59 +0200

    Made interactive, faster by using sets instead of lists & only show possible end positions instead of all possible routes

--- main.py
+++ main.py
@@ -1,49 +1,54 @@
 
 from generate_connections import generate_connections
 
-board: dict[int, dict[str, set[int]]] = generate_connections()
 
-start_pos = int(input("Start position: "))
-transports = []
-while(True):
-    transport_type = input("Transport type: ")
-    if transport_type == "":
-        break
-    if transport_type not in ['taxi', 'bus', 'metro', 'hide']:
-        print("Invalid transport type. Please enter 'taxi', 'bus', 'metro' or 'hide'.")
-        continue
-    transports.append(transport_type)
-
-
-def recurse_route(transports: list[str], routes: list[list[int]], first_recursive_call = True) -> list[list[int]]:
-    for i in range(len(routes)):
-        current_pos = routes[i][-1]
+def find_nodes(transports: list[str], current_nodes: set[int]) -> set[int]:
+    new_nodes: set[int] = set()
+    for current_node in current_nodes:
         current_transport = transports[0]
         next_nodes: set[int] = set()
         if current_transport == 'hide':
-            next_nodes.update(board[current_pos].get('taxi', set()))
-            next_nodes.update(board[current_pos].get('bus', set()))
-            next_nodes.update(board[current_pos].get('metro', set()))
-            next_nodes.update(board[current_pos].get('water', set()))
+            next_nodes.update(board[current_node].get('taxi', set()))
+            next_nodes.update(board[current_node].get('bus', set()))
+            next_nodes.update(board[current_node].get('metro', set()))
+            next_nodes.update(board[current_node].get('water', set()))
         else:
-            next_nodes = board[current_pos].get(current_transport, set())
+            next_nodes = board[current_node].get(current_transport, set())
 
-        for next_node in next_nodes:
-            routes.append(routes[i].copy())
-            routes[-1].append(next_node)
-            if len(transports) > 1:
-                for new_route in recurse_route(transports[1:], [routes[-1]], False):
-                    routes.append(new_route)
+        if len(transports) > 1:
+            new_nodes.update(find_nodes(transports[1:], next_nodes))
+        else:
+            new_nodes.update(next_nodes)
 
-    if first_recursive_call:
-        result = []
-        for route in routes:
-            if len(route) == len(transports) + 1:
-                result.append(route)
-        return result
+    return new_nodes
 
-    return routes
-    
 
-for route in recurse_route(transports, [[start_pos]]):
-    print(route)
+board: dict[int, dict[str, set[int]]] = generate_connections()
+
+def main():
+    nodes: set[int] = {int(input("Start position: "))}
+    while(True):
+        transports = []
+        transport_type = input("Transport type: ")
+        if transport_type == "":
+            break
+        if transport_type not in ['taxi', 'bus', 'metro', 'hide']:
+            print("Invalid transport type. Please enter 'taxi', 'bus', 'metro' or 'hide'.")
+            continue
+        transports.append(transport_type)
+
+        nodes = find_nodes(transports, nodes)
+        list_nodes = list(nodes)
+        if len(list_nodes) <= 0:
+            print("Transport not possible!")
+            transports.pop()
+            continue
+
+        list_nodes.sort()
+        print("Possible locations:")
+        for node in list_nodes:
+            print(node)
+
+
+if __name__ == "__main__":
+    main()
